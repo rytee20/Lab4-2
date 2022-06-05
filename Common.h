e08b3fe4abd65352bbc8162f0e019c4eeb84faa2
rytee20 <94058290+rytee20@users.noreply.github.com>
2022-06-05 18:33:35 +0700

    Create Common.h

---
+++ Common.h
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <Windows.h>
+#include <locale>
+
+using namespace std;
+
+class Common
+{
+	friend class Box;
+
+protected:
+
+	char* name;
+
+public:
+
+	Common();
+
+	Common& operator=(const Common& equal);
+
+	virtual void add_data();
+	virtual void show_data();
+};
